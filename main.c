892ffcd4d9566521a7aa2d0d2cb72b173a1bf2b4
leaa06 <tasslea81@gmail.com>
2026-03-30 14:43:08 +0300

    added team members' names

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "game.h"
 
 int main() {
+printf("==============================\n");
+printf("     DOTS AND BOXES GAME\n");
+printf("==============================\n");
+printf("Team Members:\n");
+printf("-Majd Al Halabi\n");
+printf("-Lea Tass\n");
+printf("==============================\n");
     start_game();
     return 0;
 }
