90e165188b9bc68225913449f3940e96e3eaf19c
majd-alhalabi <majd.halabi9@gmail.com>
2026-04-14 12:16:35 +0300

    Added getters and valid move checker

--- board.h
+++ board.h
@@ -7,8 +7,12 @@
 void init_board();
 void print_board();
 
-int draw_line(int r1, int c1, int r2, int c2, char player);
+int draw_line(int r1,int c1,int r2,int c2,char player);
+int is_valid_move(int r1,int c1,int r2,int c2);
 int is_game_over();
 int get_score(char player);
+
+int get_h_line(int i, int j);
+int get_v_line(int i, int j);
 
 #endif
