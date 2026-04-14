1c9cf1e794edc3e54544ee4763a7732abefd2c0e
majd-alhalabi <majd.halabi9@gmail.com>
2026-04-14 12:27:17 +0300

    Added getters and valid move checker

--- board.c
+++ board.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "board.h"
 
 static int h_lines[ROWS+1][COLS];
@@ -41,44 +40,64 @@ void print_board() {
             for(int j = 0; j < COLS + 1; j++) {
                 if(v_lines[i][j]) printf("|");
                 else printf(" ");
-                if(j < COLS) printf(" %c ", boxes[i][j]);
+                if(j < COLS) {
+                    printf(" %c ", boxes[i][j]);
+                }
             }
             printf("\n");
         }
     }
 }
 
-int check_box(int r, int c, char player) {
-    if(boxes[r][c] != ' ') return 0;
+int is_valid_move(int r1,int c1,int r2,int c2) {
 
+    if(r1 < 0 || r1 > ROWS || c1 < 0 || c1 > COLS) return 0;
+    if(r2 < 0 || r2 > ROWS || c2 < 0 || c2 > COLS) return 0;
+
+    if(r1 == r2 && (c1 == c2+1 || c1 == c2-1)) {
+        int c = (c1 < c2) ? c1 : c2;
+        if(h_lines[r1][c] == 0) return 1;
+    }
+
+    if(c1 == c2 && (r1 == r2+1 || r1 == r2-1)) {
+        int r = (r1 < r2) ? r1 : r2;
+        if(v_lines[r][c1] == 0) return 1;
+    }
+
+    return 0;
+}
+
+int check_box(int r,int c,char player) {
     if(h_lines[r][c] && h_lines[r+1][c] &&
-       v_lines[r][c] && v_lines[r][c+1]) {
+       v_lines[r][c] && v_lines[r][c+1] &&
+       boxes[r][c] == ' ') {
         boxes[r][c] = player;
         return 1;
     }
     return 0;
 }
 
-int draw_line(int r1, int c1, int r2, int c2, char player) {
+int draw_line(int r1,int c1,int r2,int c2,char player) {
+
+    if(!is_valid_move(r1,c1,r2,c2)) return -1;
+
     int scored = 0;
 
-    if(r1 == r2 && abs(c1 - c2) == 1) {
+    if(r1 == r2) {
         int c = (c1 < c2) ? c1 : c2;
-        if(h_lines[r1][c]) return -1;
         h_lines[r1][c] = 1;
 
-        if(r1 > 0) scored += check_box(r1-1, c, player);
-        if(r1 < ROWS) scored += check_box(r1, c, player);
+        if(r1 < ROWS) scored += check_box(r1,c,player);
+        if(r1 > 0) scored += check_box(r1-1,c,player);
     }
-    else if(c1 == c2 && abs(r1 - r2) == 1) {
+
+    if(c1 == c2) {
         int r = (r1 < r2) ? r1 : r2;
-        if(v_lines[r][c1]) return -1;
         v_lines[r][c1] = 1;
 
-        if(c1 > 0) scored += check_box(r, c1-1, player);
-        if(c1 < COLS) scored += check_box(r, c1, player);
+        if(c1 < COLS) scored += check_box(r,c1,player);
+        if(c1 > 0) scored += check_box(r,c1-1,player);
     }
-    else return -1;
 
     return scored;
 }
@@ -86,14 +105,22 @@ int draw_line(int r1, int c1, int r2, int c2, char player) {
 int is_game_over() {
     for(int i=0;i<ROWS;i++)
         for(int j=0;j<COLS;j++)
-            if(boxes[i][j]==' ') return 0;
+            if(boxes[i][j] == ' ') return 0;
     return 1;
 }
 
 int get_score(char player) {
-    int score=0;
+    int score = 0;
     for(int i=0;i<ROWS;i++)
         for(int j=0;j<COLS;j++)
-            if(boxes[i][j]==player) score++;
+            if(boxes[i][j] == player) score++;
     return score;
+}
+
+int get_h_line(int i, int j) {
+    return h_lines[i][j];
+}
+
+int get_v_line(int i, int j) {
+    return v_lines[i][j];
 }
